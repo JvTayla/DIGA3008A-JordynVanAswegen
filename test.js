9be9ee0ad897fe8416d71d07b78342b13d9d6b4b
jetayla <vanaswegen.jordy@gmail.com>
2025-05-13 09:16:11 +0200

    Idk

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log("this is my console message");
+
+
+//Exercise 1
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+const length = 10;
+const width = 5;
+console.log("Exercise 1: Area of rectangle =", calculateRectangleArea(length, width));
+
+//Exercise 2
+//Write a JavaScript program to compute the sum of the two integers. If the two values are same, then returns triple their sum. 
+
+
+
+//exercise 3 
