b935cf5e86da4838ce31020122b22ef6a917158f
jetayla <vanaswegen.jordy@gmail.com>
2025-05-22 22:13:45 +0200

    Fixed Home page - no more extra H1

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("this is my console message");
-
-function calculateRectangleArea(length, width) {
-    return length * width;
-}
-
-const length = 10;
-const width = 5;
-console.log("Exercise 1: Area of rectangle =", calculateRectangleArea(length, width));
