40ecda01f691ca6b5c3c111bc711d9b921bef506
jetayla <vanaswegen.jordy@gmail.com>
2025-05-23 22:23:44 +0200

    Blogs Updated

---
+++ back-to-top.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+window.onscroll = function () {
+  const btn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+}
+);
