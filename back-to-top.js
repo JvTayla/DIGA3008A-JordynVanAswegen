52b3f958d20c8c340cfb0867e2f3ce357aeac627
jetayla <vanaswegen.jordy@gmail.com>
2025-05-23 22:36:36 +0200

    Back to Top JS

--- back-to-top.js
+++ back-to-top.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-window.onscroll = function () {
-  const btn = document.getElementById("backToTopBtn");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-};
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("backToTopBtn");
 
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-}
-);
+    // Show or hide the button on scroll
+    window.onscroll = function () {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    };
+
+    // Handle click event to scroll to top
+    btn.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  });
